0b145729e3bb47989a72469dcadb973d84e8ccee
David Falk <david.falk@vr-smart-finanz.de>
2019-04-17 08:51:41 +0200

    atbash completed

--- java/atbash-cipher/src/main/java/Atbash.java
+++ java/atbash-cipher/src/main/java/Atbash.java
@@ -1,6 +1,29 @@
+/**
+ * Atbash
+ */
 public class Atbash {
+  static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  private void encodeChars(char[] input) {
+    for (int index = 0; index < input.length; index += 1) {
+      int charIndex = alphabet.indexOf(input[index]);
+      if (charIndex != -1) {
+        input[index] = alphabet.charAt(alphabet.length() - charIndex - 1);
+      }
+    }
+  }
 
   String encode(String input) {
-    return input;
+    char[] onlyLetters = input.toLowerCase().replaceAll("\\W", "").toCharArray();
+    encodeChars(onlyLetters);
+    String asString = new String(onlyLetters);
+    String addSpaces = asString.replaceAll("(.{5})(?=.)", "$1 ");
+    return addSpaces;
+  }
+
+  String decode(String input) {
+    char[] onlyLetters = input.replaceAll(" ", "").toCharArray();
+    encodeChars(onlyLetters);
+    return new String(onlyLetters);
   }
 }
